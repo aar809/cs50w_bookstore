79aec2a9cff960c318959e8c3c3b4aa8c56b8daa
Aaron Chan <a.chan809@gmail.com>
2020-06-09 17:01:11 -0700

    first commit

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ app = Flask(__name__)
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 @app.route('/')
 def index():
